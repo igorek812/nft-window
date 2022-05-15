406ed416a6a58c370c2598cc7f6d9120c50940cb
Igor <>
2022-05-15 22:42:57 +0300

    add script

---
+++ build/precache-manifest.fd77b96b745268c15647b38a9e164e0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75375f9168592f7b92a86282750cb0a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d50abac2eebe5b25ea7",
+    "url": "/static/css/main.517dd1b0.chunk.css"
+  },
+  {
+    "revision": "442aab1b5701e6220fdb",
+    "url": "/static/js/2.ad212304.chunk.js"
+  },
+  {
+    "revision": "6d50abac2eebe5b25ea7",
+    "url": "/static/js/main.9fc89b8c.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
